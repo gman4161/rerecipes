3adb6c274e3852f8aaeee54ca915083bc96d73e6
engelgarrett <gmanrulesalot@gmail.com>
2023-11-25 22:58:49 -0600

    further progress into homepage

--- index.php
+++ index.php
@@ -9,6 +9,7 @@ All the basic default methods or lines will be explained in this file only
     to be on the page*/
 include "login.php"
 ?>
+<?php $longtext = "Simply a test of how long i can make this line because the line needs to be really long for me to see what is going to happen when it reaches the end of the line but also i can't just no see what happens when the line is really long because then i would not know how long the line could be or if the line goes off the page or if the line would just go to the next line and look nice just the way it is but without testing it first, i wouldn't know so i have to write this long sentance to figure out what exactly will happen otherwise i would be in the dark and lay awake at night now knowing what is or could be" ?>
 <?php
 //this line ensures a session is running
 //the session stores variables like user sign in information
@@ -28,40 +29,36 @@ if(!session_id()) session_start();
         some other important information
     -->
     <?php include("header.php");?>
-    <body>
-        <main class="addPadding">
-            <h3>Welcome!</h3>
-            Here 
-        </main>
-        <form class="addPadding" method="POST" action="./php/login.php">
-            <fieldset id="login">
-                <legend>Sign in</legend>
-                <!--Make the input line up properly-->
-                <table class="login">
-                    <tr>
-                        <td><label for="username">Username:</label></td>
-                        <td><input type="text" name="username" id="username"<?= isset($_SESSION["Username"]) ? "value='{$_SESSION['Username']}'" : ""?>></td>
-                    </tr>
-                    <tr>
-                        <td><label for="password">Password:</label></td>
-                        <td><input type="password" name="password" id="password"></td>
-                    </tr>
-                    <tr>
-                        <td style="text-align: left;"><button type="submit" value="Log in">Log in</button></td>
-                        <td>
-                            <p style="color: red;">
-                            <?php
-                                //this will print an error message for why sign in failed
-                                if(isset($_SESSION['reason'])){
-                                    echo $_SESSION['reason'];
-                                    unset($_SESSION['reason']);
-                                }
-                            ?>
-                            </p>
-                        </td>
-                    </tr>
-                </table>
-            </fieldset>
-        </form>
-    </body>
+    <main class="addPadding">
+        <h1>Welcome to ReRecipes!</h1>
+<!-- 
+    Swap this out with a FOREACH loop after querying the DB
+
+    Figure out how to dynamically load these as the page scrolls
+    (maybe like a 'hidden' class and a # class for each item then
+    remove the hidden class for the next 10 # class)
+-->    
+        <?php for($i=1; $i<=10; $i++){ ?>
+        <div class="module">
+            <h2 style="display: inline">Recipe <?=$i?> title   </h2><small> by [Author <?=$i?>]</small>
+            <p><i>Short description: <?= $longtext?> <?=$i?>[more button?]</i></p>
+            <ul>
+                <li>
+                    possible ingredient list
+                </li>
+                <li>
+                    [potentially after more button/separate more button.]
+                </li>
+                <li>
+                    [list left to right, maybe measurements]
+                </li>
+                <li>
+                    <?= $longtext ?>
+                </li>
+            </ul>
+            <small><?=$i?> steps</small>
+        </div>
+        <?php } ?>
+
+    </main>
 </html>
